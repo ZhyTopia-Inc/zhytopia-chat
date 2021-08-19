6beedbe168e0abba7c0f057e1206a6eab971cd24
Eternity <66208385+EternityGT@users.noreply.github.com>
2021-08-19 16:02:49 +0700

    Update loginForm.jsx

--- src/components/loginForm.jsx
+++ src/components/loginForm.jsx
@@ -35,7 +35,7 @@ const Modal = () => {
           <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} className="input" placeholder="Password" required />
           <div align="center">
             <button type="submit" className="button">
-              <span>Start chatting</span>
+              <span>LOGIN</span>
             </button>
           </div>
         </form>
